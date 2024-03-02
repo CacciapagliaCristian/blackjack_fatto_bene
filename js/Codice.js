8d0a42dc406f0158ac13447bd35a5544ed753c45
unknown <c.cacciapaglia.2223@vallauri.net>
2024-03-02 11:40:59 +0100

    ù

---
+++ js/Codice.js
@@ -0,0 +1,18 @@
+function carica_denaro(){
+    window.location.href="../html/Carica_denaro.html";
+}
+function settings(){
+    window.location.href="../html/Settings.html";
+
+}
+function gioca(){
+    window.location.href="../html/Campo_da_gioco.html";
+
+}
+function home(){
+    window.location.href="../html/Home.html";
+
+}
+function vis_grafica(){
+
+}
